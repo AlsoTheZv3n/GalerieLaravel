1bca9c485b5ddc53311951301c2886bf5985dcc0
Sven Weidenmann <sweidenmann@gmail.com>
2022-06-08 10:57:52 +0200

    second commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return view('galerie');
 });
+
+Route::get('/' , [GaleriesController::class, 'index']);
+Route::get('/galeriesOverview/{id}' , [GaleriesController::class, 'view'])->middleware('auth', 'readauth');
+Route::get('/edit/{id}' , [GaleriesController::class, 'edit'])->middleware('auth', 'editauth');
+Route::put('/edit/{id}' , [GaleriesController::class, 'update'])->middleware('auth', 'editauth');
+
+Route::get('/galeriesDetailOverview/{id}' , [GaleriesController::class, 'view'])->middleware('auth', 'readauth');
+Route::get('/edit/{id}' , [GaleriesController::class, 'edit'])->middleware('auth', 'editauth');
+Route::put('/edit/{id}' , [GaleriesController::class, 'update'])->middleware('auth', 'editauth');
+
+Route::get('/galeriesview/{id}' , [GaleriesController::class, 'view'])->middleware('auth', 'readauth');
+Route::get('/edit/{id}' , [GaleriesController::class, 'edit'])->middleware('auth', 'editauth');
+Route::put('/edit/{id}' , [GaleriesController::class, 'update'])->middleware('auth', 'editauth');
+
+
+Route::get('/galerieEdit/{id}' , [GaleriesController::class, 'index']);
+Route::get('/galerie' , [GaleriesController::class, 'create']);
+
+
+Route::get('/admin' , [AdminsController::class, 'index']);
+Route::get('/admins/{admin}' , [AdminsController::class, 'index']);
+Route::get('/admins/{admin}/edit' , [AdminsController::class, 'edit']);
+Route::get('/admins/{admin}/destroy' , [AdminsController::class, 'destroy']);
+
+Route::get('/users/{id}', [UsersController::class, 'show']);
+
+
+
+
+

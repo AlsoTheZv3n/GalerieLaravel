f96cf11f8ef364f1b8adee33c10781d7fb36d329
Sven Weidenmann <sweidenmann@gmail.com>
2022-06-09 11:43:32 +0200

    third commit

--- resources/views/galeriesOverview.blade.php
+++ resources/views/galeriesOverview.blade.php
@@ -1,1 +1,34 @@
-//Home von den alle gallerien von authoren (Tumbnail)
+@extends('layout')
+
+@section('content')
+
+    <main role="main">
+
+        <section class="jumbotron text-center">
+            <div class="container col-md-7">
+            <h1 class="text text-light fs-2 fw-bold m-4">Galeries  </h1>
+                <div class="row">
+                    @foreach ($Galeries as $Galerie)
+                        <div class="col-4">
+                            <div class="card mb-4 box-shadow bg-dark text-light border-secondary">
+                                <img src="/Bilder/Galerie/Bild1.jpg" class="card-img-top" alt="Galerie">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $Galerie->name }}</h5>
+                                    <p class="card-text">{{ $Photo->name }}</p>
+                                    <p class="card-text">{{ $Galerie->autor }}</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="btn-group">
+                                            <td><a href="/galeriesOverviewView/{{ $Galerie->id }}" class="btn btn-primary">View</a></td>
+                                            <td><a href="/galeriesOverviewEdit/{{ $Galerie->id }}" class="btn btn-warning">Edit</a></td>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </section>
+    </main>
+
+@endsection
